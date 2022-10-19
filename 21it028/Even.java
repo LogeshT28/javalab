a74822fc74283da41545111983cda7c193f1e4f1
Logesh__28 <115222518+logesh028@users.noreply.github.com>
2022-10-19 12:37:54 +0530

    Add files via upload

---
+++ 21it028/Even.java
@@ -0,0 +1,18 @@
+package E;
+public class Even
+	{
+		public void Number(int a)
+
+		{
+			
+		if(a%2==0)
+		{
+				
+			System.out.println(a+" is even NUmber");
+		}
+		else
+		{
+			System.out.println(a+" is odd NUmber");
+		}
+	}
+	}
